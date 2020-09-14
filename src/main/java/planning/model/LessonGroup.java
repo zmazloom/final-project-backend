2ca5060bf863d069a51aae516cae0720960b1f11
zmazloom94@gmail.com <lhnvlk110>
2020-09-15 02:33:29 +0430

    add lesson group

---
+++ src/main/java/planning/model/LessonGroup.java
@@ -0,0 +1,30 @@
+package planning.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "lesson_group")
+public class LessonGroup {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(updatable = false)
+    @CreationTimestamp
+    private Date created;
+
+    @Column
+    private boolean removed;
+
+}
